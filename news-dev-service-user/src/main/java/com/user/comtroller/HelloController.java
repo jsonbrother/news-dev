2dc98b6f28613b314429b91106ea007bbbddb4af
json <tong_haijun@htaonet.com>
2021-01-15 13:45:21 +0800

    配置logback日志

--- news-dev-service-user/src/main/java/com/user/comtroller/HelloController.java
+++ news-dev-service-user/src/main/java/com/user/comtroller/HelloController.java
@@ -1,6 +1,8 @@
 package com.user.comtroller;
 
 import com.api.comtroller.user.HelloControllerApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,7 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController implements HelloControllerApi {
 
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     public Object hello() {
+
+        logger.debug("debug: hello~");
+        logger.info("info: hello~");
+        logger.warn("warn: hello~");
+        logger.error("error: hello~");
+
         return "hello";
     }
 
