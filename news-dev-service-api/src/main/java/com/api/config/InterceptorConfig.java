a0c9afe005ae7b3e0c664abd9146792845d59a54
json <tong_haijun@htaonet.com>
2021-01-17 09:07:37 +0800

    用户发送短信拦截并限制60秒

---
+++ news-dev-service-api/src/main/java/com/api/config/InterceptorConfig.java
@@ -0,0 +1,26 @@
+package com.api.config;
+
+import com.api.interceptors.PassportInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by TongHaiJun
+ * 2021/1/17 8:55
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PassportInterceptor passportInterceptor() {
+        return new PassportInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor())
+                .addPathPatterns("/passport/getSMSCode");
+    }
+}
---
+++ news-dev-service-api/src/main/java/com/api/interceptors/PassportInterceptor.java
@@ -0,0 +1,67 @@
+package com.api.interceptors;
+
+import com.utils.IPUtil;
+import com.utils.RedisOperator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by TongHaiJun
+ * 2021/1/17 8:41
+ */
+public class PassportInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private RedisOperator redis;
+
+    private static final String MOBILE_SMSCODE = "mobile:smscode";
+
+
+    /**
+     * 请求访问到controller之前
+     *
+     * @param request  请求对象
+     * @param response 响应对象
+     * @param handler  拦截程序
+     * @return true：放行 false：拦截
+     * @throws Exception 异常信息
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // 1.获取用户ip
+        String userIp = IPUtil.getRequestIp(request);
+
+        // 2.判断redis是否存在key
+        boolean keyIsExist = redis.keyIsExist(MOBILE_SMSCODE + ":" + userIp);
+
+        // 3.存在则拦截
+        if (keyIsExist) {
+            // TODO 待优化 返回错误信息
+            System.out.println("用户ip：" + userIp + "短信发送频率太快");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 请求访问到controller之后 渲染视图之前
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 请求访问到controller之后 渲染视图之后
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
